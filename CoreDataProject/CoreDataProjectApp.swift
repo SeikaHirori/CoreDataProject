fc8524f0127566612752364470ae5a341f6f6e6e
SeikaHirori <SeikaHirori@outlook.com>
2023-06-09 14:30:30 -0700

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Seika Hirori on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
