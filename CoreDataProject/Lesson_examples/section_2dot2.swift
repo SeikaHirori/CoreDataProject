803f05285f34faea4f7977156d4bc36c83c5ce96
SeikaHirori <SeikaHirori@outlook.com>
2023-08-01 18:24:06 -0700

    Started section 2.2; added new entity and subclass "Singer"

---
+++ CoreDataProject/Lesson_examples/section_2dot2.swift
@@ -0,0 +1,21 @@
+//
+//  section_2dot2.swift
+//  CoreDataProject
+//
+//  Created by Seika Hirori on 8/1/23.
+//
+
+import CoreData
+import SwiftUI
+
+struct section_2dot2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct section_2dot2_Previews: PreviewProvider {
+    static var previews: some View {
+        section_2dot2()
+    }
+}
---
+++ Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Seika Hirori on 8/1/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
