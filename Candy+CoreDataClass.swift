0397ec88046d4f7c72a3329f64c9f6b31ba10777
SeikaHirori <SeikaHirori@outlook.com>
2023-08-04 12:36:18 -0700

    Created subclass for entities "Candy" and "Country"

---
+++ Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Seika Hirori on 8/4/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Seika Hirori on 8/4/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
